6e782edd3924c98eb4b35f32ed9da64963119d0a
ray <rraymondxie@gmail.com>
2025-01-11 20:11:40 -0800

    created memory game on web

---
+++ lang/messages/en/user.js
@@ -0,0 +1,7 @@
+const messages = {
+    INVALID_INPUT: "Please enter a number between 3 and 7.",
+    EXCELLENT_MEMORY: "Excellent memory!",
+    WRONG_ORDER: "Wrong order! Try again.",
+};
+
+export default messages;
